fb6ce7337dc9bf9b5a03b3fbade77ee61bf99509
Jarek Pendowski <jarek@pendowski.com>
2020-03-08 19:34:07 +0100

    Plugin: Provides currently playing info via websocket:6001

---
+++ plugins/current-playing/front.js
@@ -0,0 +1,44 @@
+function findCurrentPlaying() {
+    const infoElement = document.querySelector('.content-info-wrapper.style-scope.ytmusic-player-bar')
+    const pauseElement = document.querySelector('#play-pause-button')
+    if (!infoElement || !pauseElement) {
+        return null
+    }
+    const title = infoElement.querySelector('.title')
+    const subtitle = infoElement.querySelector('.subtitle .byline')
+    let result = {
+        title: '',
+        artist: '',
+        isPlaying: false
+    }
+
+    if (title) {
+        result.title = title.innerText
+    }
+    if (subtitle) {
+        result.artist = subtitle.innerText
+    }
+    if (pauseElement.getAttribute('aria-label') === 'Pause') {
+        result.isPlaying = true
+    }
+    return JSON.stringify(result)
+}
+
+function run() {
+    const WebSocket = require("ws")
+    const wss = new WebSocket.Server({ port: 6001 })
+    wss.on('connection', function (w) {
+        w.on('message', function (data) {
+            if (data != '') {
+                w.send(findCurrentPlaying())
+            }
+            console.log(data)
+        })
+        w.on('close', function () {
+            console.log("Closed")
+        })
+        w.send(findCurrentPlaying())
+    })
+}
+
+module.exports = run
